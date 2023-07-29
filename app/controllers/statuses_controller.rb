1b436f13b2fa00b47f0ef890bd6186958239d66f
Kento210 <T121015@ed.sus.ac.jp>
2023-07-29 03:51:44 +0000

    応答がユニークになるよう、distinctメゾットを実装

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,8 +1,10 @@
 class StatusesController < ApplicationController
   skip_before_action :verify_authenticity_token
-    def index
-      @statuses = Status.all
-      render json: { data: @statuses }, status: :ok
-    end
+
+  def index
+    @statuses = Status.select(:tagId, :stepNo, :boothId, :operator, :content, :created_at).distinct
+    render json: { data: @statuses }, status: :ok
   end
+end
+
   
