b3d04faed61137da6bf68e60f0cd1fb5be813383
Kento210 <T121015@ed.sus.ac.jp>
2023-08-03 22:17:04 +0000

    stepNoを更新できるように変更

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,21 +5,24 @@ class TagsController < ApplicationController
     tags_params = params["_json"] || [params.require(:tag).permit(:tagId, :stepNo, :boothId, :operator, :content)]
 
     tags_params.each do |tag_param|
-      @tag = Tag.new(tag_param.permit(:tagId, :stepNo, :boothId, :operator, :content))
+      @tag = Tag.find_or_initialize_by(tagId: tag_param[:tagId])
 
-      if @tag.save
-        # タグが正常に保存された後、その情報をStatusモデルにも保存します。
-        @status = Status.new(tag_param.permit(:tagId, :stepNo, :boothId, :operator, :content).merge(created_at: @tag.created_at))
-        unless @status.save
-          render json: { error: 'Failed to create status' }, status: :unprocessable_entity
+      if @tag.new_record? || @tag.stepNo != tag_param[:stepNo]
+        @tag.assign_attributes(tag_param.permit(:stepNo, :boothId, :operator, :content))
+
+        if @tag.save
+          # タグが正常に保存された後、その情報をStatusモデルにも保存します。
+          @status = Status.new(tag_param.permit(:tagId, :stepNo, :boothId, :operator, :content).merge(created_at: @tag.created_at))
+          unless @status.save
+            render json: { error: 'Failed to create status' }, status: :unprocessable_entity
+            return
+          end
+        else
+          render json: { error: 'Failed to create or update tag' }, status: :unprocessable_entity
           return
         end
       else
-        if @tag.errors.details[:tagId].any? { |detail| detail[:error] == :taken }
-          render json: { error: 'TagId and stepNo combination already exists Error:400' }, status: :bad_request
-        else
-          render json: { error: 'Failed to create tag' }, status: :unprocessable_entity
-        end
+        render json: { error: 'TagId and stepNo combination already exists Error:400' }, status: :bad_request
         return
       end
     end
@@ -27,4 +30,5 @@ class TagsController < ApplicationController
     render json: { message: 'OK', created_at: @tag.created_at }, status: :ok
   end
 end
+
 
